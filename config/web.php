1c2fb51900ec60c1c2a773f6b68c6813135b4ee2
shtadler <andrushtadler@gmail.com>
2017-02-08 18:24:42 +0200

    add htaccess to web

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
+            'baseUrl' => '/',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '3vKDbpbErZdCldtVPQPT84fSBLbfN1WK',
         ],
