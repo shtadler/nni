8db2fc671de50a70e4beabbd3e2d4cf4228e3dfe
shtadler <andrushtadler@gmail.com>
2017-02-10 00:06:40 +0200

    requre swiper widget and add to site/index (students)

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,6 @@
+<?php
+use romkaChev\yii2\swiper\Swiper;
+?>
 <section id="main-slider" class="carousel">
     <div class="carousel-inner">
         <div class="item active">
@@ -28,91 +31,76 @@
                 <h2>Студентам</h2>
             </div><!--/.center-->
             <div class="row">
-                <div class="col-md-4 col-sm-6">
+                <?php
+                echo Swiper::widget( [
+                    'items'         => [
+                        '<div class="">
                     <div class="center">
                         <i class="icon-apple icon-md icon-color1"></i>
                         <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
-                    <div class="center">
-                        <i class="icon-android icon-md icon-color2"></i>
-                        <h4>Android development</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
-                    <div class="center">
-                        <i class="icon-windows icon-md icon-color3"></i>
-                        <h4>Windows Phone development</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
-                    <div class="center">
-                        <i class="icon-html5 icon-md icon-color4"></i>
-                        <h4>Ruby on Rails development</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
-                    <div class="center">
-                        <i class="icon-css3 icon-md icon-color5"></i>
-                        <h4>Javascript development</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-                    </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
-                    <div class="center">
-                        <i class="icon-thumbs-up icon-md icon-color6"></i>
-                        <h4>Responsive web design</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
-                    </div>
-                </div><!--/.col-md-4-->
-            </div><!--/.row--><div class="row">
-                <div class="col-md-4 col-sm-6">
+                </div>',
+                        '<div class="">
                     <div class="center">
                         <i class="icon-apple icon-md icon-color1"></i>
                         <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
+                </div>', '<div class="">
                     <div class="center">
-                        <i class="icon-android icon-md icon-color2"></i>
-                        <h4>Android development</h4>
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
+                </div>',
+                        '<div class="">
                     <div class="center">
-                        <i class="icon-windows icon-md icon-color3"></i>
-                        <h4>Windows Phone development</h4>
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
+                </div>', '<div class="">
                     <div class="center">
-                        <i class="icon-html5 icon-md icon-color4"></i>
-                        <h4>Ruby on Rails development</h4>
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
+                </div>',
+                        '<div class="">
                     <div class="center">
-                        <i class="icon-css3 icon-md icon-color5"></i>
-                        <h4>Javascript development</h4>
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
-                <div class="col-md-4 col-sm-6">
+                </div>', '<div class="">
                     <div class="center">
-                        <i class="icon-thumbs-up icon-md icon-color6"></i>
-                        <h4>Responsive web design</h4>
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
                         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
                     </div>
-                </div><!--/.col-md-4-->
+                </div>',
+                        '<div class="">
+                    <div class="center">
+                        <i class="icon-apple icon-md icon-color1"></i>
+                        <h4>iOS development</h4>
+                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae.</p>
+                    </div>
+                </div>',
+                    ],
+                    'behaviours'    => [
+                        Swiper::BEHAVIOUR_NEXT_BUTTON,
+                        Swiper::BEHAVIOUR_PREV_BUTTON
+                    ],
+                    'pluginOptions' => [
+                        Swiper::OPTION_SLIDES_PER_VIEW      => 3,
+                        Swiper::OPTION_PAGINATION_CLICKABLE => true,
+                        Swiper::OPTION_SPACE_BETWEEN        => 30,
+                        Swiper::OPTION_LOOP                 => true,
+                        Swiper::OPTION_SLIDES_PER_COLUMN => 2
+                    ]
+                ] );
+                ?>
             </div><!--/.row-->
         </div><!--/.box-->
     </div><!--/.container-->
