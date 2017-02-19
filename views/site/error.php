525b4f223094877bce4ffaa08e4cd5adbfa8477b
shtadler <andrushtadler@gmail.com>
2017-02-19 20:50:13 +0200

    fix error page

--- views/site/error.php
+++ views/site/error.php
@@ -9,29 +9,12 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
-
 <section class="first">
     <div class="container">
         <div class="box">
 
             <div class="center gap">
-                <h1><?= Html::encode($model->title) ?></h1>
+                <h1><?= Html::encode($this->title) ?></h1>
             </div><!--/.center-->
 
             <h3><?= nl2br(Html::encode($message)) ?></h3>
